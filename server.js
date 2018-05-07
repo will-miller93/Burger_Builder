cba5465f4fa9d171d2acb4ee0c3ac25b683bb8f4
Will Miller <willmiller@Wills-MacBook-Pro.local>
2018-05-07 15:07:55 -0400

    working port issue

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 // set PORT
-var PORT = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 // new express instance
 var app = express();
@@ -27,6 +27,6 @@ var routes = require("./controllers/burgers_controllers");
 
 app.use(routes);
 
-app.listen(PORT, function() {
+app.listen(port, function() {
   console.log("App now listening at localhost:" + PORT);
 });
